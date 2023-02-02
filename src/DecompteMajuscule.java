1e20202339aed366298e77aa8455d91e84f58ee2
Vick-17 <vega.victor.0712@gmail.com>
2023-02-02 15:25:22 +0100

    third push, VerifMaj

---
+++ src/DecompteMajuscule.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DecompteMajuscule {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Rentrer une phrase avec des majuscule");
+        String entrer = scan.nextLine();
+
+        int maj = nbrDeMaj(entrer);
+
+        System.out.print(maj);
+
+    }
+    private static int nbrDeMaj(String chaine) {
+        int compteur=0;
+        for(int i = 0; i<chaine.length(); i++){
+            char ch = chaine.charAt(i);
+            if(Character.isUpperCase(ch))
+                compteur++;
+        }
+        return compteur;
+    }
+}
